6728c26ab51918fe9a84e90a8b4c6e134d732b1c
Apekshahande <46122496+Apekshahande@users.noreply.github.com>
2019-03-18 23:31:11 +0530

    Add files via upload

---
+++ kbc.py
@@ -0,0 +1,13 @@
+print('kaun banega crorpati')
+print('the fist question is now in front of you')
+print('Q.which mountain had the peak befor the discovery of the mount everest')
+print('your options are:\n1).mount everest\n2).ketu\n3).mount fuzi\n4).none of them')
+x = input('enter your choice')
+a = 1
+b = 2
+c = 3
+d =  4
+if x == 4:
+	print('you are right')
+else:
+	print('sorry you are wrong the answer is none of them')
